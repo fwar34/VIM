de242e7b290abec146acc0cef70263891fd0e294
liang.feng <fwar34@126.com>
2017-11-09 13:14:23 +0800

    modify

--- cp.sh
+++ cp.sh
@@ -7,7 +7,8 @@
 #!/bin/bash
 
 cp ~/.vimrc ~/learn/VIM/vimrc
-#cp ~/.vim/syntax/c.vim ~/learn/VIM/syntax/
+cp ~/.vim/syntax/c.vim ~/learn/VIM/syntax/
+cp ~/.vim/syntax/cpp.vim ~/learn/VIM/syntax/
 cp ~/.vim/colors/wombat256.vim ~/learn/VIM/colors/
 cp ~/.vim/colors/codeschool.vim ~/learn/VIM/colors/
 

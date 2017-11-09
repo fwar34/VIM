66d051a2b8029bd6b3a89a605a9733a9282a5936
liang.feng <fwar34@126.com>
2017-11-09 13:31:50 +0800

    s

--- cp.sh
+++ cp.sh
@@ -6,9 +6,44 @@
 #########################################################################
 #!/bin/bash
 
-cp ~/.vimrc ~/learn/VIM/vimrc
-cp ~/.vim/syntax/c.vim ~/learn/VIM/syntax/
-cp ~/.vim/syntax/cpp.vim ~/learn/VIM/syntax/
-cp ~/.vim/colors/wombat256.vim ~/learn/VIM/colors/
-cp ~/.vim/colors/codeschool.vim ~/learn/VIM/colors/
+push()
+{
+	cp ~/.vimrc ./vimrc
+	cp ~/.vim/syntax/c.vim ./syntax/
+	cp ~/.vim/syntax/cpp.vim ./syntax/
+	cp ~/.vim/colors/wombat256.vim ./colors/
+	cp ~/.vim/colors/codeschool.vim ./colors/
+}
 
+pull()
+{
+	cp ./vimrc ~/.vimrc
+
+	if [ -d "~/.vim" ]
+	then
+		if [ ! -d "~/.vim/syntax" ]
+		then
+			mkdir -p ~/.vim/syntax
+		fi
+
+		if [ ! -d "~/.vim/colors" ]
+		then
+			mkdir -p ~/.vim/colors
+		fi
+	else
+		mkdir -p ~/.vim/syntax
+		mkdir -p ~/.vim/colors
+	fi
+
+	cp ./syntax/c.vim ~/.vim/syntax/
+	cp ./syntax/cpp.vim ~/.vim/syntax/
+	cp ./colors/wombat256.vim ~/.vim/colors/
+	cp ./colors/codeschool.vim ~/.vim/colors/
+}
+
+if [ $1 == "push" ]
+then
+	push
+else
+	pull
+fi

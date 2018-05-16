2382d9529bd989e80d776cab6c2e355fdd858efe
fwar34 <fwar34@126.com>
2018-05-16 17:29:12 +0800

    fix conemu

--- emacs_msys2.bat
+++ emacs_msys2.bat
@@ -1,3 +1,3 @@
-rem E:\msys64\usr\bin\mintty.exe /usr/bin/zsh.exe --login -i -c /mingw64/bin/runemacs.exe
-E:\msys64\mingw64.exe /usr/bin/zsh.exe --login -i -c /mingw64/bin/runemacs.exe
+E:\msys64\usr\bin\mintty.exe /usr/bin/zsh.exe --login -i -c /mingw64/bin/runemacs.exe
+rem E:\msys64\mingw64.exe /usr/bin/zsh.exe --login -i -c /mingw64/bin/runemacs.exe
 rem E:\msys64\mingw32.exe /usr/bin/zsh.exe --login -i -c /mingw64/bin/runemacs.exe

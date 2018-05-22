542476f5acab13c8047ecd725306c2a92aa65f91
fwar34 <fwar34@126.com>
2018-05-22 20:35:16 +0800

    fix vim_install.sh

--- vim_install.sh
+++ vim_install.sh
@@ -2,18 +2,20 @@
 
 os=$(uname -a|awk -F_ '{print $1}')
 #os=$(MSYSTEM) #just define in msys
+echo $os
 
-if test $os = 'MSYS' -o $os = 'CYGWIN'
+#if test "$os" = 'MSYS' -o "$os" = 'CYGWIN'
+#if [ "$os" = 'MSYS' -o "$os" = 'CYGWIN' ]
+if [ "$os" = 'MSYS' ] || [ "$os" = 'CYGWIN' ]
 then
     if [ ! -d ~/vimfiles ]
     then
         mkdir -p ~/vimfiles/autoload/
-        cd ~/vimfiles/autoload/
-        wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -O ~/vimfiles/autoload/plug.vim
     else
         if [ ! -f ~\vimfiles\autoload\plug.vim ]
         then
-            wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+            wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -O ~/vimfiles/autoload/plug.vim
         fi
     fi
 else
@@ -29,10 +31,9 @@ then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
-if [ ! -f ~\downloads\global-6.6.2.tar.gz ]
+if [ ! -f ~/downloads/global-6.6.2.tar.gz ]
 then
-    cd ~\downloads
-    wget http://tamacom.com/global/global-6.6.2.tar.gz
+    wget http://tamacom.com/global/global-6.6.2.tar.gz -O ~/downloads/global-6.6.2.tar.gz
 fi
 
 if [ -f ~/.vimrc ]
@@ -41,7 +42,7 @@ then
 fi
 ln -s ~/mine/VIM/vimrc.vim-plug ~/.vimrc
 
-if test $os = 'MSYS' -o $os = 'CYGWIN'
+if test "$os" = 'MSYS' -o "$os" = 'CYGWIN'
 then
     if [ -f ~/_vimrc ]
     then
@@ -67,3 +68,9 @@ then
 	mv ~/.gitconfig ~/.gitconfig.bak
 fi
 ln -s ~/mine/VIM/git/gitconfig ~/.gitconfig
+
+if [ -f ~/.agignore ]
+then
+	mv ~/.agignore ~/.agignore.bak
+fi
+ln -s ~/mine/VIM/agignore ~/.agignore

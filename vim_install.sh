d25753b9ebb578a082e4505c22e33c8e3006c476
fwar34 <fwar34@126.com>
2020-03-13 17:41:25 +0800

    add pacman comment

--- vim_install.sh
+++ vim_install.sh
@@ -328,3 +328,34 @@ echo Complete
 #compile emacs with x window
 #sudo apt install libgtk-3-dev
 #sudo apt-get install libxpm-dev libjpeg-dev libgif-dev libtiff5-dev libgnutls28-dev
+
+#////////////////////////////////////////////////////////////////////
+#https://www.jianshu.com/p/ea651cdc5530
+#pacman
+#删除无用的包pacman -Rns $(pacman -Qdtq)
+#安装包
+
+#➔ pacman -S 包名：例如，执行 pacman -S firefox 将安装 Firefox。你也可以同时安装多个包，
+#只需以空格分隔包名即可。
+#➔ pacman -Sy 包名：与上面命令不同的是，该命令将在同步包数据库后再执行安装。
+#➔ pacman -Sv 包名：在显示一些操作信息后执行安装。
+#➔ pacman -U：安装本地包，其扩展名为 pkg.tar.gz。
+#➔ pacman -U http://www.example.com/repo/example.pkg.tar.xz 安装一个远程包（不在 pacman 配置的源里面）
+#删除包
+
+#➔ pacman -R 包名：该命令将只删除包，保留其全部已经安装的依赖关系
+#➔ pacman -Rs 包名：在删除包的同时，删除其所有没有被其他已安装软件包使用的依赖关系
+#➔ pacman -Rsc 包名：在删除包的同时，删除所有依赖这个软件包的程序
+#➔ pacman -Rd 包名：在删除包时不检查依赖。
+#搜索包
+
+#➔ pacman -Ss 关键字：在仓库中搜索含关键字的包。
+#➔ pacman -Qs 关键字： 搜索已安装的包。
+#➔ pacman -Qi 包名：查看有关包的详尽信息。
+#➔ pacman -Ql 包名：列出该包的文件。
+#其他用法
+
+#➔ pacman -Sw 包名：只下载包，不安装。
+#➔ pacman -Sc：清理未安装的包文件，包文件位于 /var/cache/pacman/pkg/ 目录。
+#➔ pacman -Scc：清理所有的缓存文件
+#////////////////////////////////////////////////////////////////////

56728ebcbbac205beec89a5a5e7c6d9ec5341deb
fwar34 <fwar34@126.com>
2020-12-16 20:51:30 +0800

    update

--- proxy.sh
+++ proxy.sh
@@ -1,6 +1,6 @@
 hostip=$(grep nameserver /etc/resolv.conf|awk '{print $2}')
 http_port=7890
 socks5_port=7891
-export https_proxy="https://${hostip}:${http_port}"
-export http_proxy="http://${hostip}:${http_port}"
+# export https_proxy="https://${hostip}:${http_port}"
+# export http_proxy="http://${hostip}:${http_port}"
 export all_proxy="socks5://${hostip}:${socks5_port}"

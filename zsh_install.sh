9ed66f17d4ea5e4c5669ada17a83658e27459449
fwar34 <fwar34@126.com>
2021-09-30 16:09:34 +0800

    update

--- zsh_install.sh
+++ zsh_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-apt install zsh autojump
+sudo apt install zsh autojump
 if [[ ! -d ~/.oh-my-zsh ]]; then
-    #sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    #sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi

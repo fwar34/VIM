c970f75c1dca96e8bccece03afd32ddb2081f219
fwar34 <fwar34@126.com>
2022-07-15 22:19:29 +0800

    update

--- zshenv.sh
+++ zshenv.sh
@@ -192,9 +192,21 @@ if [[ $MYHOSTNAME == "ubuntu-awesome" ]]; then
     export XMODIFIERS=@im=fcitx
     export QT_IM_MODULE=fcitx
 else
-    export GTK_IM_MODULE=fcitx5
-    export QT_IM_MODULE=fcitx5
-    export XMODIFIERS=@im=fcitx5
+    # export GTK_IM_MODULE=fcitx5
+    # export QT_IM_MODULE=fcitx5
+    # export XMODIFIERS=@im=fcitx5
+
+    # export GTK_IM_MODULE=fcitx
+    # export QT_IM_MODULE=fcitx
+    # export XMODIFIERS=@im=fcitx
+    # export GLFW_IM_MODULE=ibus
+
+    export GTK_IM_MODULE=fcitx
+    export QT_IM_MODULE=fcitx
+    export XMODIFIERS=@im=fcitx
+    export INPUT_METHOD=fcitx
+    export SDL_IM_MODULE=fcitx
+    export GLFW_IM_MODULE=ibus
 fi
 
 # }}}

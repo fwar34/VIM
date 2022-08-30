9206b7cc749e383588a5d9a9c539ef69e2953433
fwar34 <fwar34@126.com>
2022-08-30 17:44:00 +0800

    update

--- vim_install.sh
+++ vim_install.sh
@@ -86,7 +86,7 @@ if [[ ${os} == "ubuntu" ]] || [[ ${os} == "debian" ]] || [[ ${os} == "elementary
 elif [[ ${os} == 'ManjaroLinux' ]] || [[ ${os} == 'arch' ]]; then
     sudo pacman -Sy base-devel curl wget zsh tmux fzf the_silver_searcher fd figlet ripgrep fd python go unzip \
          thefuck global tig cmake universal-ctags bat tldr python-pip librime emacs neovim inetutils rustup ranger archlinux-keyring gvfs \
-         net-tools dnsutils inetutils iproute2
+         net-tools dnsutils inetutils iproute2 tcpdump
     # gui
     sudo pacman -S picom thunar feh rofi
     # vscode sync

2066e4e8e2a9945bcd571a63cc212b6bbe38979f
fwar34 <fwar34@126.com>
2022-08-30 12:42:45 +0800

    update

--- vim_install.sh
+++ vim_install.sh
@@ -85,7 +85,8 @@ if [[ ${os} == "ubuntu" ]] || [[ ${os} == "debian" ]] || [[ ${os} == "elementary
     sudo apt install thunar feh rofi
 elif [[ ${os} == 'ManjaroLinux' ]] || [[ ${os} == 'arch' ]]; then
     sudo pacman -Sy base-devel curl wget zsh tmux fzf the_silver_searcher fd figlet ripgrep fd python go unzip \
-         thefuck global tig cmake universal-ctags bat tldr python-pip librime emacs neovim inetutils rustup ranger archlinux-keyring gvfs
+         thefuck global tig cmake universal-ctags bat tldr python-pip librime emacs neovim inetutils rustup ranger archlinux-keyring gvfs \
+         net-tools dnsutils inetutils iproute2
     # gui
     sudo pacman -S picom thunar feh rofi
     # vscode sync

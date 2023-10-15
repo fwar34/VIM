640da0fa4dab4c8a5cc832a489790f7a3fecf33c
fwar34 <fwar34@126.com>
2023-10-15 17:02:01 +0800

    add ranger

--- vim_install.sh
+++ vim_install.sh
@@ -89,7 +89,7 @@ if [[ ${os} == "ubuntu" ]] || [[ ${os} == "debian" ]] || [[ ${os} == "elementary
         sudo apt update && apt install -y silversearcher-ag
     fi
     sudo apt install -y curl wget build-essential zsh tmux libncurses5-dev \
-         python3-pip cmake autoconf pkg-config fzf ripgrep universal-ctags autojump golang lm-sensors
+         python3-pip cmake autoconf pkg-config fzf ripgrep universal-ctags autojump golang lm-sensors ranger
     # gui
     #sudo apt install thunar feh rofi
 elif [[ ${os} == 'ManjaroLinux' ]] || [[ ${os} == 'arch' ]]; then
